953fc5072f29a608ce1e0a1327761927e5c440a5
acer <a23300750@ceti.mx>
2026-04-12 19:59:25 -0600

    Correción del llamado de las funciones. Se añádieron las funciones incluyendo encriptación (para nombre de usuario, correo y contraseña) en Supabase

--- sesionusuario.js
+++ sesionusuario.js
@@ -7,11 +7,11 @@ const SUPABASE_ANON_KEY = 'sb_publishable_iYJmQZxGd4GCDIWtqUWRew_llrsfDSH'; //Es
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 // --- ELEMENTOS DE INTERFAZ ---
-const contenedor     = document.getElementById('panel-principal');
+const contenedor = document.getElementById('panel-principal');
 const btnIrARegistro = document.getElementById('activar-registro');
-const btnIrALogin    = document.getElementById('activar-login');
-const formRegistro   = document.getElementById('form-registro');
-const formLogin      = document.getElementById('form-login');
+const btnIrALogin = document.getElementById('activar-login');
+const formRegistro = document.getElementById('form-registro');
+const formLogin = document.getElementById('form-login');
 
 /**
  * CONTROL DE ANIMACIÓN
@@ -31,22 +31,22 @@ btnIrALogin.addEventListener('click', () => {
 formRegistro.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const email    = document.getElementById('reg-email').value.trim();
+    const email = document.getElementById('reg-email').value.trim();
     const password = document.getElementById('reg-password').value;
     const username = document.getElementById('reg-username').value.trim();
 
     const { error } = await supabaseClient.rpc('registrar_usuario', {
         p_username: username,
-        p_clave:    password,
-        p_correo:   email
+        p_clave: password,
+        p_correo: email
     });
 
     if (error) {
-        alert('Error al registrarse: ' + error.message);
+        alert('Error: ' + error.message);
         return;
     }
 
-    alert('¡Registro exitoso! Ya puedes iniciar sesión.');
+    alert('Registro exitoso');
     formRegistro.reset();
     contenedor.classList.remove('estado-registro-activo');
 });
@@ -58,16 +58,16 @@ formRegistro.addEventListener('submit', async (e) => {
 formLogin.addEventListener('submit', async (e) => {
     e.preventDefault();
 
-    const email    = document.getElementById('log-email').value.trim();
+    const email = document.getElementById('log-email').value.trim();
     const password = document.getElementById('log-password').value;
 
     const { data, error } = await supabaseClient.rpc('login_usuario', {
         p_correo: email,
-        p_clave:  password
+        p_clave: password
     });
 
     if (error) {
-        alert('Error de acceso: ' + error.message);
+        alert('Error: ' + error.message);
         return;
     }
 
