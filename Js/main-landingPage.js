d6d0541d084f2f215a1364b44cebb318a1838b3f
micmil3000 <a23300749@ceti.mx>
2026-04-26 03:34:33 -0600

    Cambios en la sección de servicios, creación de la seccion de faqs + estilo. Casi terminada. Cambios a Implementar: - Agregar Js para abrir y cerrar preguntas como acordeón - Redireccionar a página de Tablón - Sección de servicios redireccionar a página de registro - Sección de servicios agregar botón y mensaje cta

--- Js/main-landingPage.js
+++ Js/main-landingPage.js
@@ -1,3 +1,7 @@
 import './heroAnimation.js';
 import './characteristicsTabs.js'
 import './solutionVideoObserver.js'
+
+document.addEventListener("DOMContentLoaded", () => {
+    lucide.createIcons();
+});
