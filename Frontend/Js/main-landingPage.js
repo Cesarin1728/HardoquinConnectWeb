e3ce95cc792252c34f68af06558028aa9ec75b77
micmil3000 <a23300749@ceti.mx>
2026-04-28 00:32:41 -0600

    Cambios finales para que la nvabar ya se cargue sola. Además agregue un estilo diferente en la navbar para detectar que pestaña es la que está abierta

--- Frontend/Js/main-landingPage.js
+++ Frontend/Js/main-landingPage.js
@@ -2,9 +2,9 @@ import {initHero} from './heroAnimation.js';
 import {initUI} from './ui-landingPage.js';
 import { initNavbar } from './navbar.js';
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
     initHero();
     initUI();
-    initNavbar();
+    await initNavbar();
     lucide.createIcons();
 });
--- Frontend/Js/navbar.js
+++ Frontend/Js/navbar.js
@@ -1,8 +1,45 @@
-export function initNavbar(){
-    const userBtn = document.querySelector('.nav__list_item--user-info');
+async function createNavbar(){
+    const url = './Components/navbar.html';
+    const navbarContainer = document.getElementById('main__navbar');
+
+    const response = await fetch(url);
+    const html = await response.text();
+
+    console.log(html);
+    navbarContainer.innerHTML = html;
+}
+
+function setUpDropdownEvents(){
+    const userBtn = document.querySelector('.nav__user');
     const userDropdown = document.querySelector('.nav__user_dropdown');
 
-    userBtn.addEventListener('click', () =>{
+    userBtn.addEventListener('click', (e) =>{
+        e.stopPropagation();
         userDropdown.classList.toggle('nav__user_dropdown--active');
     })
+
+    document.addEventListener('click', (e) => {
+        const clickInside = userBtn.contains(e.target) || userDropdown.contains(e.target)
+        if(!clickInside){
+            userDropdown.classList.remove('nav__user_dropdown--active');
+        }
+    })
+}
+
+function setUpTabEvents(){
+    const navItems = document.querySelectorAll('.nav__list_item:not(.nav__list_item--user-info)');
+    navItems.forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            navItems.forEach(itm => {
+                itm.classList.remove('nav__list_item--active');
+            })
+            navItem.classList.add('nav__list_item--active');
+        })
+    })
+}
+
+export async function initNavbar(){
+    await createNavbar();
+    setUpDropdownEvents();
+    setUpTabEvents();
 }
