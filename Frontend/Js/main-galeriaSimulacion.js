1670b77b28572ee33036e448572eba8e622a436a
micmil3000 <a23300749@ceti.mx>
2026-05-25 00:17:20 -0600

    Ya quedó creado el resultado de la simulación.

---
+++ Frontend/Js/main-galeriaSimulacion.js
@@ -0,0 +1,7 @@
+import { initNavbar } from "./navbar.js"; 
+import { activeTab } from "./navbar.js";
+
+document.addEventListener("DOMContentLoaded", async () =>{
+    await initNavbar();
+    activeTab('simulaciones');
+});
